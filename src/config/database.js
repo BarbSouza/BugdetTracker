9d0d9fda605e9f8be5512fee6bf26d4a195f6f4d
Barbs28 <2023059@student.cct.ie>
2025-01-06 21:06:35 +0000

    Database and userController Java Files
    adding the database.js and userController.js

---
+++ src/config/database.js
@@ -0,0 +1,81 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+// Establishes a connection to the MySQL database using environment variables
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    multipleStatements: true, // Allows multiple SQL statements in a single query
+  });
+
+
+  function initDB(){
+
+    connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`, (err) => {
+        if (err) {
+            console.error("Error creating database");
+                throw err;
+        } else {
+            console.log("Databse created");
+        }
+
+        connection.query("USE BudgetTracker_DB", (err) => {
+            if(err) {
+                console.error("Error using the created DB");
+                throw err;
+            } else {
+                console.log("Using the new Database");
+            }
+        });
+
+        const createWalletTable = `CREATE TABLE IF NOT EXISTS Wallets (
+            wallet_id INT AUTO_INCREMENT PRIMARY KEY,
+            wallet_name VARCHAR(255) NOT NULL,
+            initial_balance DOUBLE NOT NULL
+        );`;
+
+            connection.query(createWalletTable, function (err){
+                if (err){
+                    console.error("Error creating Wallets table");
+                } else {
+                    console.log("Wallets Table Created")
+                }
+            });
+
+        const createCategoriesTable = `CREATE IF NOT EXISTS Categories (
+            category_id INT AUTO_INCREMENT PRIMARY KEY,
+            category_name VARCHAR(255) NOT NULL
+        );`;
+
+        connection.query(createCategoriesTable, function (err){
+            if (err) {
+                console.error("Error creating Categories table");
+                throw err;
+            } else {
+                console.log("Categories Table Created")
+            }
+        });
+
+        const createTransactionsTable = `CREATE IF NOT EXISTS Transactions (
+            transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+            date DATE NOT NULL,
+            type VARCHAR(255) NOT NULL,
+            wallet_id INT NOT NULL,
+            category_id INT NOT NULL,
+            description VARCHAR (255),
+            amount DOUBLE NOT NULL,
+            FOREIGN KEY (wallet_id) REFERENCES Wallets(wallet_id) ON DELETE CASCADE,
+            FOREIGN KEY (category_id) REFERENCES Categories(category_id) ON DELETE CASCADE
+        );`;
+        connection.query(createTransactionsTable, function(err){
+            if (err){
+                console.error("Error creating Transactions table");
+            } else {
+                console.log("Transaction Table Created");
+            }
+        });
+    });
+  };
+
+  module.exports = { connection, initDB}
---
+++ src/controllers/userControllers.js
@@ -0,0 +1,109 @@
+const path = require("path");
+const { connection } = require("../config/database");
+
+const addWallet = (req, res) => {
+    const { walletName, initialBalance } = req.body;
+
+    const query = "INSERT INTO Wallets (wallet_name, initial_balance) VALUES (?, ?)";
+    connection.query(query, [walletName, initialBalance], (err) => {
+        if (err) {
+            console.error("Error adding wallet:", err);
+            return res.status(500).send("Error adding wallet");
+        }
+        res.sendFile(path.join(__dirname, "../frontend/wallets.html"))
+    });
+};
+
+const addCategory = (req, res) => {
+    const { categoryName } = req.body;
+
+    const query = "INSERT INTO Categories (category_name) VALUES (?)";
+    connection.query(query, [categoryName], (err) => {
+        if (err) {
+            console.error("Error adding category:", err);
+            return res.status(500).send("Error adding category");
+        }
+        
+        res.sendFile(pathjoin(__dirname, "../frontend/categories.html"));
+
+    });
+};
+
+const addTransaction = (req, res) => {
+    const { date, type, wallet_id, category_id, description, amount} = req.body;
+
+    const query = "INSERT INTO transactions (date, type, wallet_id, category_id, description, amount) VALUES (?, ?, ?, ?, ?, ?)";
+
+    connection.query(query, [date, type, wallet_id, category_id, description, amount], (err) => {
+        if (err) {
+            console.error("Error inserting transaction:", err);
+        }
+        res.sendFile(path.join(__dirname, "../frontend/transactions.html"));
+    });
+
+};
+
+const getTransactions = (req, res) => {
+    const query = `SELECT t.transaction_id, t.date, t.type, w.wallet_name AS Wallet, c.category_name AS Category, t.description, t.amount
+    FROM Transactions t
+    LEFT JOIN Wallets w ON t.wallet_id = w.wallet_id
+    LEFT JOIN Categories c ON t.category_id = c.category_id`;
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error("Error fetching transactions", err);
+            res.status(500).send("Error fetching transactions");
+        } else {
+            console.log("Fetched Transactions:", results);
+            res.json(results);
+        }
+    });
+};
+
+const deleteTransaction = (req, res) => {
+    const transactionID = req.params.id;
+    const query = `DELETE FROM Transactions WHERE transaction_id = ?`;
+    connection.query(query, [transactionId], (err, result) => {
+        if (err){
+            console.error("Error deleting transaction:", err);
+            res.status(500).send("Error deleting transaction");
+        } else {
+            res.status(200).send("Transaction deleted successfully");
+        }
+    })
+
+}
+
+const getWallets = (req, res) => {
+    const query = "SELECT wallet_id, wallet_name FROM Wallets";
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error("Error fetching wallets:", err);
+            return res.status(500).send("Erros fecthing wallets");
+        } else {
+            res.json(results);
+        }
+    });
+};
+
+const getCategories = (req, res) => {
+    const query = "SELECT category_id, category_name FROM Categories";
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error("Error fetching categories:", err);
+            return res.status(500).send("Erros fecthing categories");
+        } else {
+            res.json(results);
+        }
+    });
+};
+
+module.exports = {
+    addTransaction,
+    getTransactions,
+    deleteTransaction,
+    addWallet,
+    addCategory,
+    getCategories,
+    getWallets
+};
